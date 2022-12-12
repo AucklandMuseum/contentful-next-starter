c9cfa978bc4797f62f221bd8bb134b3c57b060ea
Max Gilbert <mgilbert@aucklandmuseum.com>
2022-12-12 13:46:05 +1300

    il8n

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const { withPlugins } = nextComposePlugins.extend(() => ({}));
  * documentation: https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
 module.exports = withPlugins(plugins, {
-  i18n,
+  /*i18n,*/
   /**
    * add the environment variables you would like exposed to the client here
    * documentation: https://nextjs.org/docs/api-reference/next.config.js/environment-variables
